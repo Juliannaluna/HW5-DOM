de00f5c60383976a43c0a1043b2c088e04e59010
Juliannaluna <juliannakan@ukr.net>
2022-04-11 12:00:00 +0300

    fixed

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+const name = document.querySelector('#username');
+const pass = document.querySelector('#pass');
+
 function showInfo() {
-    const name = document.querySelector('#username');
-    const pass = document.querySelector('#pass');
     console.log(name.value);
     console.log(pass.value);
 }
 
 function saveToLocal() {
-    const name = document.querySelector('#username');
-    const pass = document.querySelector('#pass');
     // let user = {username: name.value, password: pass.value}
     // localStorage.setItem('user', JSON.stringify(user));
     localStorage.setItem('username', name.value);
